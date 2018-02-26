a6bd0834e7ec138a9447c9abe7d1c41d00617296
Matthew <matthew.lai@laipac.com>
2018-02-25 23:21:50 -0500

    Add BLE mesh 1.0 folder
    Add BLE mesh 1.0 folder

--- ble_mesh_v0.9/runit.bat
+++ ble_mesh_v0.9/runit.bat
@@ -1,2 +1,1 @@
-set MDG_HOME=C:/mdg/common
 ..\bin\tclsh runit.tcl C:\mdg\ble_mesh\Obj C:\mdg\ble_mesh\List 
--- ble_mesh_v0.9/runit_scg.bat
+++ ble_mesh_v0.9/runit_scg.bat
@@ -1,2 +1,1 @@
-set MDG_HOME=C:/mdg/common
 ..\bin\tclsh runit_scg.tcl C:\mdg\ble_mesh\List
